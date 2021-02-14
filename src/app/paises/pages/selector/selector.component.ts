f76fc132412c204c68e66918bc2a7397ac3b9360
Abraham <isolisrmz@gmail.com>
2021-02-14 13:33:41 -0600

    Fin de la seccion de formularios reactivos con selectores anidados

--- src/app/paises/pages/selector/selector.component.ts
+++ src/app/paises/pages/selector/selector.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+import { Pais } from '../../interfaces/pais';
+import { switchMap, tap } from 'rxjs/operators'
+import { CountryResponse } from '../../interfaces/country-response';
 
 @Component({
   selector: 'app-selector',
@@ -7,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = [];
+  paises: Pais[] = [];
+  fronteras: Pais[] = [];
+  form!: FormGroup;
+  cargando: boolean = false;
+  constructor(private fb: FormBuilder, private paisesService: PaisesService) { }
 
   ngOnInit(): void {
+    this.buildForm();
+    this.allRegions();
+    this.allCountries();
+    this.getCountry();
   }
 
+  buildForm(){
+    this.form = this.fb.group({
+      region: ['', Validators.required], 
+      pais: ['', Validators.required], 
+      fronteras: ['', Validators.required]
+    })
+  }
+
+  guardar(){
+    console.log(this.form.value);
+  }
+
+  allRegions(){
+    this.regiones = this.paisesService.regions;
+  }
+
+  allCountries(){
+    this.form.get('region')?.valueChanges
+    .pipe(
+      //Transforma la respuesta que se recibe desde la api
+      tap( (_) => {
+        this.form.get('pais')?.reset('');
+        this.cargando = true;
+      }),
+      //Este operador dispara dos observables 
+      switchMap( region => this.paisesService.allCountries(region) )
+    )
+    .subscribe( paises => {
+      this.cargando = false;
+      this.paises = paises;
+    })
+  }
+  //Fronteras
+  getCountry(){
+    this.form.get('pais')?.valueChanges
+    .pipe(
+      tap( () => {
+        //Purga el campo de fronteras cada vez que se hace una nueva petición
+        this.form.get('fronteras')?.reset('');
+        this.cargando = true;
+      }),
+      switchMap( codigo => this.paisesService.getCountry(codigo)),
+      switchMap(pais => this.paisesService.getBorder(pais?.borders!))
+    )
+    .subscribe( paises => {
+      this.cargando = false;
+      this.fronteras = paises;
+    })
+  }
 }
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { combineLatest, Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { CountryResponse } from '../interfaces/country-response';
+import { Pais } from '../interfaces/pais';
+const base_url = environment.base_url;
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  constructor(private http: HttpClient) { }
+
+  allCountries(region: string): Observable<Pais[]>{
+    const url = `${base_url}/region/${region}?fields=alpha3Code;name`;
+    return this.http.get<Pais[]>(url);
+  }
+
+  get regions(){
+    //copia desestructurada para que no se modifique la propiedad original
+    return [...this._regiones];
+  }
+
+  getCountry(code: string): Observable<CountryResponse | null>{
+    if(!code){
+      return of(null);
+    }
+    const url = `${base_url}/alpha/${code}`;
+    return this.http.get<CountryResponse>(url);
+  }
+
+  getCountry2(code: string): Observable<Pais>{
+    const url = `${base_url}/alpha/${code}?fields=alpha3Code;name`;
+    return this.http.get<Pais>(url);
+  }
+
+  getBorder(borders: string[]): Observable<Pais[]>{
+    if(!borders){
+      return of([]);
+    }
+
+    const peticiones: Observable<Pais>[] = [];
+    borders.forEach(codigo => {
+      const peticion = this.getCountry2(codigo);
+      peticiones.push(peticion);
+    });
+
+    return combineLatest( peticiones );
+  }
 }
